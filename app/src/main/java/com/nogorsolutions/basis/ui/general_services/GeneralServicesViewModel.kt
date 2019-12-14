7b749b41a30eb4fa753c6c5d27ae1f66fd195b23
raselnsl <rasel@nogorsolutions.com>
2019-12-14 11:32:51 +0600

    added general services fragment

---
+++ app/src/main/java/com/nogorsolutions/basis/ui/general_services/GeneralServicesViewModel.kt
@@ -0,0 +1,7 @@
+package com.nogorsolutions.basis.ui.general_services
+
+import androidx.lifecycle.ViewModel
+
+class GeneralServicesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/nogorsolutions/basis/ui/service/ServiceFrag.kt
+++ app/src/main/java/com/nogorsolutions/basis/ui/service/ServiceFrag.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 
 import com.nogorsolutions.basis.R
+import com.nogorsolutions.basis.databinding.FragServiceBinding
 
 class ServiceFrag : Fragment() {
 
@@ -15,19 +19,35 @@ class ServiceFrag : Fragment() {
         fun newInstance() = ServiceFrag()
     }
 
+    private lateinit var binding: FragServiceBinding
     private lateinit var viewModel: ServiceViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.frag_service, container, false)
+
+         binding = DataBindingUtil.inflate(
+                     inflater,
+                     R.layout.frag_service,
+                     container,
+                     false
+                 )
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ServiceViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(ServiceViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.cvGeneralService.setOnClickListener {
+            findNavController().navigate(R.id.generalServicesFragment)
+        }
     }
 
 }
