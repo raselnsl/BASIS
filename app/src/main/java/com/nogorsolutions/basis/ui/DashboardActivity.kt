d38c3c87b3506492c8ebfd3d00f2020f4237dc61
AlokBiswas <alokbsl15@gmail.com>
2019-12-22 19:30:20 -0800

    Dashboard Design

--- app/src/main/java/com/nogorsolutions/basis/ui/DashboardActivity.kt
+++ app/src/main/java/com/nogorsolutions/basis/ui/DashboardActivity.kt
@@ -1,11 +1,15 @@
 package com.nogorsolutions.basis.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.navigation.Navigation
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
@@ -13,22 +17,36 @@ import com.nogorsolutions.basis.R
 
 class DashboardActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_dash_board)
+        setContentView(R.layout.activity_dashboard)
 
-      /*  val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val toolbar = findViewById<Toolbar>(R.id.toolbarDashBoard)
         setSupportActionBar(toolbar)
 
-      val  navController = findNavController(R.id.nav_host_fragment)
-      val  nav_view = findViewById<NavigationView>(R.id.nav_view)
+        navController = findNavController(R.id.nav_host_dashboard)
+      val  nav_view = findViewById<NavigationView>(R.id.nav_view_dashboard)
+      val  drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
 
-      val  appBarConfiguration = AppBarConfiguration(
+        appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_DashBoard
-            )
+            ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        nav_view.setupWithNavController(navController)*/
+        nav_view.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_home, menu)
+
+        return true
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
